87208126bd2e2f0a91456523662b919f1dac4db5
RedDeathAt614 <mail@michaelreibelboesen.dk>
2014-12-10 14:38:27 +0100

    Subscribe done

--- api/controllers/MailchimpController.js
+++ api/controllers/MailchimpController.js
@@ -5,12 +5,29 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var MCapi = require('mailchimp-api');
+MC        = new MCapi.Mailchimp('4cf0247d56b6ff76096779ab80b712b9-us9');
+
 module.exports = {
 
 	subscribe: function(req,res,next){
-		console.log("Subscribe:",req.params.all());
 
-		res.view('static/index',{message: "Thank you for signing up - you're the best!"});
+		var listID = "288eb7197d"; // (Betalist list)
+		MC.lists.subscribe({id: listID, email:{email:req.body.email}}, function(data) {
+			res.view('static/index',{message: "Thank you for signing up - you're the best!"});
+		},
+		function(error) {
+			var errText = '';
+			if (error.error) {
+				//req.session.error_flash = error.code + ": " + error.error;
+				errText = error.error;
+			} else {
+				//req.session.error_flash = 'There was an error subscribing that user';
+				errText = 'There was an error subscribing that user';
+			}
+			res.view('static/index',{message: errText});
+		});
+
 	}
 
 };
