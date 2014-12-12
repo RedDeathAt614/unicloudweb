fa477380a6ba0fabd2b8773e80b0108e90cca423
RedDeathAt614 <mail@michaelreibelboesen.dk>
2014-12-12 14:36:08 +0100

    Confirmation

--- api/controllers/MailchimpController.js
+++ api/controllers/MailchimpController.js
@@ -14,7 +14,7 @@ module.exports = {
 
 		var listID = "288eb7197d"; // (Betalist list)
 		MC.lists.subscribe({id: listID, email:{email:req.body.email}}, function(data) {
-			res.view('static/index',{message: "Thank you for signing up - you're the best!"});
+			res.view('static/index',{message: "Thank you for signing up - you're the best! <br/>Please check your inbox for a confirmation e-mail."});
 		},
 		function(error) {
 			var errText = '';
